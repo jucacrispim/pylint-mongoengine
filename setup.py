afb99aa0676a8315988557f854af11000aa98bc7
Juca Crispim <juca@poraodojuca.net>
2018-09-13 18:32:28 -0300

    Version number
    ci: skip

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='juca@poraodojuca.net',
     description='A Pylint plugin to help Pylint understand the MongoEngine',
     long_description=LONG_DESCRIPTION,
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
