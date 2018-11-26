079d5f8e38d85d2006413679757e1138b2065f2d
Juca Crispim <juca@poraodojuca.net>
2018-11-26 17:58:58 -0200

    Display proper output on failures in test suite

--- build-scripts/check_coverage.sh
+++ build-scripts/check_coverage.sh
@@ -5,9 +5,10 @@ echo "\nChecking coverage for Python code\n"
 export PYTHONPATH=$PYTHONPATH:tests/functional/;
 
 OUT=`pytest --cov=$1`;
+
 coverage=`echo "$OUT" | grep TOTAL | sed 's/TOTAL\s*\w*\s*\w*\s*\w*\s*\w\s*//g' | cut -d'%' -f1`;
 
-ERROR=`echo "$OUT" | grep Failed`
+ERROR=`echo "$OUT" | egrep 'Failed|FAILURE'`
 
 echo 'coverage was:' $coverage'%'
 
@@ -16,11 +17,11 @@ then
     if [ $coverage -eq $2 ]
     then
 	echo "But something went wrong";
-	echo "$ERROR";
+	echo "$OUT";
 	exit 1
     else
 	echo "And something went wrong"
-	echo "$ERROR";
+	echo "$OUT";
 	coverage report -m
 	exit 1
     fi
