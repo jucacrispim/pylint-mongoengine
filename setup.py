910d44c97b5783684cbff224cc940929e0f4663b
Juca Crispim <juca@poraodojuca.net>
2019-04-13 04:08:43 -0300

    Bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='juca@poraodojuca.net',
     description='A Pylint plugin to help it understand MongoEngine',
     long_description=LONG_DESCRIPTION,
-    version='0.2.3',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
